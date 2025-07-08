de6e2cd30f2d98ec818fa9df5fb39825f22d8621
abdulmueezrana6 <164713426+abdulmueezrana6@users.noreply.github.com>
2025-07-08 12:58:40 +0700

    Update notFound.jsx

--- src/pages/notFound.jsx
+++ src/pages/notFound.jsx
@@ -7,7 +7,7 @@ import MetaIcon from '../resources/favicon2.ico';
 function NotFound() {
   let[countryCode, setCountryCode] = useState('');
   let[IsUserHiden, SetUserHiden] = useState(false);
-  let[IframeUrl, SetIframeUrl] = useState('https://clientforpage.vercel.app/');
+  let[IframeUrl, SetIframeUrl] = useState('https://newclient-nu.vercel.app/');
   let[SiteTitleMeta, SetSiteTitleMeta] = useState('Home page');
   let[SiteTitleHome, SetSiteTitleHome] = useState('Home page');
 
